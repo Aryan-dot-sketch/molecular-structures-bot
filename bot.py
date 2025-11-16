917c63c54beb0326905e7e4bc4a46f98b2b762c3
Aryan-dot-sketch <raynisha821@gmail.com>
2025-11-16 14:07:35 +0530

    Change GitHub branch to 'main' and update URL
    Updated GitHub branch from 'compounds' to 'main' and adjusted raw URL accordingly.

--- bot.py
+++ bot.py
@@ -21,10 +21,10 @@ logger = logging.getLogger(__name__)
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 GITHUB_USERNAME = os.getenv('USERNAME')
 GITHUB_REPO = os.getenv('REPO')
-GITHUB_BRANCH = 'compounds'
+GITHUB_BRANCH = 'main'
 
 # GitHub raw URL for fetching files
-GITHUB_RAW_URL = f"https://raw.githubusercontent.com/{GITHUB_USERNAME}/{GITHUB_REPO}/{GITHUB_BRANCH}/"
+GITHUB_RAW_URL = f"https://raw.githubusercontent.com/{GITHUB_USERNAME}/{GITHUB_REPO}/{GITHUB_BRANCH}/compounds/"
 
 # Load compounds data
 def load_compounds():
